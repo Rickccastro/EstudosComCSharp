2369b23c826c1aa9d67e19d3a8af8ae08557cc91
ricardo <ricardoparmezancastro.dev@gmail.com>
2024-09-26 12:15:49 -0300

    Initial commit

---
+++ DelegateGenerico/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace DelegateGenerico;
+
+internal class Program
+{
+    delegate int CalculoMatematico(int a ,int b);
+
+    delegate T DelegateGenerico<T>(T a , T b);
+
+    static void Main(string[] args)
+    {
+        /*Estatico só aceita funções do tipo INT*/
+        CalculoMatematico calculo = new CalculoMatematico(Somar);
+        Console.WriteLine(calculo(1, 1));
+
+        // Generica aceita funções de quaquer tipo
+        DelegateGenerico<int> genericoInt = new DelegateGenerico<int>(Somar);
+        Console.WriteLine(genericoInt(1, 1));
+
+        // Generica aceita funções de quaquer tipo
+        DelegateGenerico<decimal> genericoDecimal = new DelegateGenerico<decimal>(SomarDecimal);
+        Console.WriteLine(genericoDecimal(1, 1));
+
+        /*Delegate do tipo Func serve para não ter a necessidade da declaração do delegate no escopo global
+         e não serve para metodos voids*/
+        Func<int, int, int> funcCalculo = (Somar);
+        Console.WriteLine(funcCalculo(1, 1));
+
+        /*Delegate do tipo Action servem para os metodos voids,podem receber parametros mas não retornam nada*/
+        Action<int> actionImprimirCalculo = (ImprimirResultado);
+        actionImprimirCalculo(funcCalculo(1,1));   
+    }
+
+    private static void ImprimirResultado(int resultado)
+    {
+        Console.WriteLine(resultado);
+    }
+
+    static int Somar(int a ,int b)
+    {
+        return a + b;
+    }
+
+    static decimal SomarDecimal(decimal a, decimal b) 
+    {
+        return a + b;
+    }
+}
---
+++ Orientação a Eventos/GerenciadorLatidos.cs
@@ -0,0 +1,36 @@
+﻿namespace Orientação_a_Eventos;
+
+public class GerenciadorLatidos
+{
+    private int _intensidadeLatido;
+
+    public event EventHandler ExcessoDecibeisEvent;
+
+    public GerenciadorLatidos()
+    {
+        _intensidadeLatido = 0;
+    }
+    public int Latir()
+    {
+        _intensidadeLatido += 10;
+        if (_intensidadeLatido > 80)
+        {
+            ExcessoDecibeisEventArgs excessoDecibeisEventArgs = new ExcessoDecibeisEventArgs
+            {
+                IntensidadeLatido= _intensidadeLatido,  
+            };
+
+            OnExcessoDecibeisHandler(excessoDecibeisEventArgs);
+        }
+
+        return _intensidadeLatido;
+    }
+
+    protected virtual void OnExcessoDecibeisHandler(EventArgs e)
+    {
+        if(ExcessoDecibeisEvent != null)
+        {
+            ExcessoDecibeisEvent(this,e);
+        }
+    }
+}
